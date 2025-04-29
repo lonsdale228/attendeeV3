d6fae434ad351edc1e25180f38fdaca6d48f5fec
bebus <v.gerashchenkoua@gmail.com>
2025-04-29 15:24:51 +0300

    fixed agree button

--- bots/zoom_bot_adapter/zoom_ui_methods.py
+++ bots/zoom_bot_adapter/zoom_ui_methods.py
@@ -35,9 +35,7 @@ class ZoomUIMethods:
             self.driver.get_screenshot_as_file(f"LambdaTestVisibleScreen_{uuid1()}.png")
 
             try:
-                agree_btn = WebDriverWait(self.driver, 10).until(
-                    EC.element_to_be_clickable((By.XPATH, "//button[normalize-space(text())='I Agree']"))
-                )
+                agree_btn = self.locate_by_id('wc_agree1')
                 agree_btn.click()
             except Exception as e:
                 logging.error(f"Agree button not found: {str(e)}")
