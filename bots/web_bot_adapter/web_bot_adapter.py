8e57fec50c6c276172fa46dacd515e76faede9cb
bebus <v.gerashchenkoua@gmail.com>
2025-04-29 15:11:53 +0300

    some changes for new hosting

--- bots/web_bot_adapter/web_bot_adapter.py
+++ bots/web_bot_adapter/web_bot_adapter.py
@@ -300,10 +300,11 @@ class WebBotAdapter(BotAdapter):
         options = uc.ChromeOptions()
 
         options.add_argument("--use-fake-ui-for-media-stream")
-        options.add_argument(f"--window-size={self.video_frame_size[0]},{self.video_frame_size[1]}")
+        # options.add_argument(f"--window-size={self.video_frame_size[0]},{self.video_frame_size[1]}")
+        options.add_argument("--window-size=1920,1080")
         options.add_argument("--no-sandbox")
         options.add_argument("--start-fullscreen")
-        # options.add_argument('--headless=new')
+        options.add_argument('--headless=new')
         options.add_argument("--disable-gpu")
         options.add_argument("--disable-extensions")
         options.add_argument("--disable-application-cache")
