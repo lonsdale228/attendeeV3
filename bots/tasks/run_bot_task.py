67a7a6998a6437feb5a46f6bef9c0e10ba768fb9
bebus <v.gerashchenkoua@gmail.com>
2025-05-07 17:13:09 +0300

    removed SoftTimeLimit entirely

--- bots/tasks/run_bot_task.py
+++ bots/tasks/run_bot_task.py
@@ -10,7 +10,7 @@ from bots.bot_controller import BotController
 logger = logging.getLogger(__name__)
 
 
-@shared_task(bind=True, soft_time_limit=3600)
+@shared_task(bind=True, soft_time_limit=86400)
 def run_bot(self, bot_id):
     logger.info(f"Running bot {bot_id}")
     bot_controller = BotController(bot_id)
